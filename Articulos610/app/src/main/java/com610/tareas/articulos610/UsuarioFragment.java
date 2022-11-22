4836b76c83549a0b24ae1b20959ede837de3510b
MartinAAcebeyL <martinaacbyl2000pro@gmail.com>
2022-11-22 18:34:36 -0400

    face 1

---
+++ Articulos610/app/src/main/java/com610/tareas/articulos610/UsuarioFragment.java
@@ -0,0 +1,105 @@
+package com.grover101.misarticulos;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.collection.LruCache;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UsuarioFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UsuarioFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public UsuarioFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment UsuarioFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UsuarioFragment newInstance(String param1, String param2) {
+        UsuarioFragment fragment = new UsuarioFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflador, ViewGroup contenedor, Bundle savedInstanceState)
+    {
+        View vista = inflador.inflate(R.layout.fragment_usuario, contenedor, false);
+        FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        TextView nombre = (TextView) vista.findViewById(R.id.nombreUsuario);
+        TextView correo = (TextView) vista.findViewById(R.id.correoUsuario);
+        TextView proveedor = (TextView) vista.findViewById(R.id.proveedorCorreo);
+
+        nombre.setText(" " + usuario.getDisplayName());
+        correo.setText(" " + usuario.getEmail());
+        proveedor.setText(" " + usuario.getProviderId());
+
+        RequestQueue colaPeticiones = Volley.newRequestQueue(getActivity() .getApplicationContext());
+        ImageLoader lectorImagenes = new ImageLoader(colaPeticiones, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(10);
+            public void putBitmap(String url, Bitmap bitmap)
+            {
+                cache.put(url, bitmap);
+            }
+            public Bitmap getBitmap(String url)
+            {
+                return cache.get(url);
+            }
+        });
+
+        // Foto de usuario
+        Uri urlImagen = usuario.getPhotoUrl();
+        if (urlImagen != null)
+        {
+            NetworkImageView fotoUsuario = (NetworkImageView) vista.findViewById(R.id.imagen);
+            fotoUsuario.setImageUrl(urlImagen.toString(), lectorImagenes);
+        }
+
+        return vista;
+    }
+}
